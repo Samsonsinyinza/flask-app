39c08877940a07084acca630c5251409f98dc4e5
Samson Sinyinza <samsoftsinyinza@gmail.com>
2025-09-10 01:33:55 -0700

    Update home page message

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello DevOps with GitHub Actions!"
+    return "Sinyinza Demonstrating DevOps with GitHub Actions!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
