3d5f4c0ae1de542a0a4b2dd676d08567f5cd0f5b
Samson Sinyinza <samsoftsinyinza@gmail.com>
2025-09-18 08:48:51 -0700

    Initial commit - Flask To-Do app

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "GROUP 2 Demonstrating DevOps with GitHub Actions! and RENDER"
+    return "Group 2 Demonstrating DevOps with GitHub Actions! and RENDER"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
